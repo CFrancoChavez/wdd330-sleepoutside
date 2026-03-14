6cfabdfc22773ef1b1e67e8936bd43acb5d0218e
Daveralphy <raphaeldaveal@gmail.com>
2026-03-14 11:38:30 +0100

    daveal

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,6 +6,41 @@ function convertToJson(res) {
   }
 }
 
+function getProjectRootFromDataUrl(dataUrl) {
+  const { pathname } = new URL(dataUrl);
+  const knownMarkers = ["/src/json/", "/json/"];
+
+  for (const marker of knownMarkers) {
+    const markerIndex = pathname.indexOf(marker);
+    if (markerIndex >= 0) {
+      return pathname.slice(0, markerIndex);
+    }
+  }
+
+  return "";
+}
+
+function normalizeAssetPath(path, projectRoot) {
+  if (!path || !path.startsWith("/")) {
+    return path;
+  }
+
+  return `${projectRoot}${path}`;
+}
+
+function normalizeProductAssets(products, sourceUrl) {
+  const projectRoot = getProjectRootFromDataUrl(sourceUrl);
+
+  return products.map((product) => ({
+    ...product,
+    Image: normalizeAssetPath(product.Image, projectRoot),
+    Brand: {
+      ...product.Brand,
+      LogoSrc: normalizeAssetPath(product.Brand?.LogoSrc, projectRoot),
+    },
+  }));
+}
+
 export default class ProductData {
   constructor(category) {
     this.category = category;
@@ -24,7 +59,8 @@ export default class ProductData {
       try {
         const response = await fetch(path);
         if (response.ok) {
-          return await response.json();
+          const products = await response.json();
+          return normalizeProductAssets(products, response.url);
         }
       } catch (e) {
         // Continue to next strategy
